b485dc0742a99ef812b00a4f323a460597f1cf21
ogdabou <ogdabou@gmail.com>
2015-02-21 12:07:53 +0100

    #2

---
+++ js/ui/Models/RawData.js
@@ -0,0 +1,25 @@
+
+/**
+ * RawData used to be sent to hadoop
+ */
+angular.module('Models')
+
+.factory('RawData', [function() {
+	var RawData = function() {
+		var creditCardNumer,
+			accoutNumer,
+			transactionType,
+			currency,
+			merchantCode,
+			foreignTnx,
+			transactionDate,
+			merchantName,
+			merchantCity,
+			merechantCountry,
+			acquiererReference,
+			ecommerce,
+			foreignTnxAmt,
+			localTaxAmt;
+	}
+}]);
+
